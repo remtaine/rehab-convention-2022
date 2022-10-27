bdf26de290663ecf6d82d089ba7459f983e22eef
Niel Benjamin Kho <benjokho@gmail.com>
2022-10-27 22:30:44 +0800

    Add traffic signs

--- pages/contacts.js
+++ pages/contacts.js
@@ -1,7 +1,9 @@
 import React from "react";
 import LayoutBasic from "../components/layouts/LayoutBasic";
 import Link from "next/link";
+import Image from "next/image";
 import Head from "next/head";
+import circles from "/public/circles.png";
 
 function contacts() {
   return (
@@ -10,9 +12,23 @@ function contacts() {
         <title>Contact Details | 22nd PGH Rehab Postgrad</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <section className="h-fit max-w-[calc(100vw-2rem)] w-fit p-12 shadow bg-fem-300 rounded-xl">
-        <h1 className="text-3xl font-bold mb-4 ">Contact Details</h1>
-        <ul className="flex flex-col gap-2">
+      <h1 className="w-full portrait:text-4xl text-5xl font-semibold text-center uppercase">
+        Contact Details
+      </h1>
+      <div className="flex justify-center">
+        <Image
+          src={circles}
+          alt={"Traffic lights"}
+          layout="fixed"
+          width="150px"
+          height="90px"
+          // objectFit="contain"
+          quality={100}
+          className="mx-auto"
+        />
+      </div>
+      <section className="h-fit max-w-[calc(100vw-2rem)] w-fit px-12 py-6 shadow bg-fem-300 rounded-xl mt-6">
+        <ul className="flex flex-col">
           <li className="">
             <span className="font-bold">Website: </span>
             <Link href="http://www.pghrehabmed.com" className="">
--- pages/tests/index.js
+++ pages/tests/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import LayoutBasic from "/components/layouts/LayoutBasic";
 import Link from "next/link";
+import Image from "next/image";
 import Head from "next/head";
-
+import circles from "/public/circles.png";
 function tests() {
   return (
     <LayoutBasic>
@@ -10,11 +11,22 @@ function tests() {
         <title>Tests | 22nd PGH Rehab Postgrad</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <section className="h-fit max-w-[calc(100vw-2rem)] w-fit p-12 shadow bg-fem-300 rounded-xl">
-        <h1 className="text-3xl font-bold mb-4 ">Tests</h1>
-
+      <h1 className="text-5xl font-bold uppercase ">Tests</h1>
+      <div className="flex justify-center">
+        <Image
+          src={circles}
+          alt={"Traffic lights"}
+          layout="fixed"
+          width="150px"
+          height="90px"
+          // objectFit="contain"
+          quality={100}
+          className="mx-auto"
+        />
+      </div>
+      <section className="h-fit max-w-[calc(100vw-2rem)] w-fit px-12 py-6 shadow bg-fem-300 rounded-xl mt-6">
         <h2 className="text-xl font-bold my-2">October 29</h2>
-        <ul className="flex flex-col gap-2">
+        <ul className="flex flex-col gap-1">
           <li className="">
             <span className="font-bold">Pre-test: </span>
             <Link href="https://forms.gle/L1kWJ71GVCYPHaUt9" className="">
@@ -32,7 +44,7 @@ function tests() {
         </ul>
 
         <h2 className="text-xl mt-6 mb-2 font-bold">November 5</h2>
-        <ul className="flex flex-col gap-2">
+        <ul className="flex flex-col gap-1">
           <li className="">
             <span className="font-bold">Pre-test: </span>
             <Link href="#" className="">
