3580e1eaaf87fe9981f7f4299527380628444c39
Niel Benjamin Kho <benjokho@gmail.com>
2022-10-27 20:03:44 +0800

    Change typo

--- pages/sponsors/hiesai.js
+++ pages/sponsors/hiesai.js
@@ -1,7 +1,7 @@
 import React from "react";
 import LayoutSponsors from "../../components/layouts/LayoutSponsors";
 import VirtualBooth from "../../components/VirtualBooth";
-import boothImage from "/public/booths/Hi_Esai.png";
+import boothImage from "/public/booths/Hi_Eisai.png";
 
 function hiEsai(props) {
   return (
